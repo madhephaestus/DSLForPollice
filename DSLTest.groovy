d50e161c7513241c39a33e563bac593abfc2ceb6
Kevin Harrington <mad.hephaestus@gmail.com>
2017-11-13 13:06:15 -0500

    Adding the interface
    Define the class from the interface

--- DSLTest.groovy
+++ DSLTest.groovy
@@ -1,1 +1,52 @@
 println "Loading new DSL..."
+
+ScriptingEngine.addScriptingLanguage(new IScriptingLanguage {
+	/**
+	 * This interface is for adding additional language support. 
+	 * @param code file content of the code to be executed
+	 * @param args the incoming arguments as a list of objects
+	 * @return the objects returned form the code that ran
+	 */
+	public abstract Object inlineScriptRun(File code, ArrayList<Object> args) throws Exception{
+		
+	}
+	
+	/**
+	 * This interface is for adding additional language support. 
+	 * @param code the text content of the code to be executed
+	 * @param args the incoming arguments as a list of objects
+	 * @return the objects returned form the code that ran
+	 * @throws Exception 
+	 */
+	public abstract Object inlineScriptRun(String code, ArrayList<Object> args) throws Exception{
+		
+	}
+	
+	/**
+	 * Returns the shell type of this language
+	 * @return
+	 */
+	public abstract String getShellType(){
+		
+	}
+	/**
+	 * This function should return true is the filename provided is of a supported file extension. 
+	 * This function may never be called if this language is only used internally. 
+	 * @param filename the filename of the file to be executed
+	 * @return true if the file extension is supported, false otherwise.
+	 */
+	public abstract boolean isSupportedFileExtenetion(String filename){
+		
+	}
+	/**
+	 * This function returns if this is a binary file or a text file
+	 * @return true if the file is a text file.
+	 */
+	public boolean getIsTextFile(){
+		
+	}
+	
+}
+
+	
+	)
