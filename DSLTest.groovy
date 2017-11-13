cc773d6ce87392ffbbda574d5d9add676c009abe
Kevin Harrington <mad.hephaestus@gmail.com>
2017-11-13 13:09:09 -0500

    Stubs implemented

--- DSLTest.groovy
+++ DSLTest.groovy
@@ -7,8 +7,8 @@ ScriptingEngine.addScriptingLanguage(new IScriptingLanguage {
 	 * @param args the incoming arguments as a list of objects
 	 * @return the objects returned form the code that ran
 	 */
-	public abstract Object inlineScriptRun(File code, ArrayList<Object> args) throws Exception{
-		
+	public  Object inlineScriptRun(File code, ArrayList<Object> args) throws Exception{
+		return null;
 	}
 	
 	/**
@@ -18,16 +18,16 @@ ScriptingEngine.addScriptingLanguage(new IScriptingLanguage {
 	 * @return the objects returned form the code that ran
 	 * @throws Exception 
 	 */
-	public abstract Object inlineScriptRun(String code, ArrayList<Object> args) throws Exception{
-		
+	public  Object inlineScriptRun(String code, ArrayList<Object> args) throws Exception{
+		return null;
 	}
 	
 	/**
 	 * Returns the shell type of this language
 	 * @return
 	 */
-	public abstract String getShellType(){
-		
+	public  String getShellType(){
+		return "Arduing Scripting Langauge"
 	}
 	/**
 	 * This function should return true is the filename provided is of a supported file extension. 
@@ -35,15 +35,18 @@ ScriptingEngine.addScriptingLanguage(new IScriptingLanguage {
 	 * @param filename the filename of the file to be executed
 	 * @return true if the file extension is supported, false otherwise.
 	 */
-	public abstract boolean isSupportedFileExtenetion(String filename){
-		
+	public  boolean isSupportedFileExtenetion(String filename){
+		if(	filename.toLowerCase().endsWith(".asl")||
+				)
+			return true;
+		return false;
 	}
 	/**
 	 * This function returns if this is a binary file or a text file
 	 * @return true if the file is a text file.
 	 */
 	public boolean getIsTextFile(){
-		
+		return true;
 	}
 	
 }
